963fdb100ac4b01c437052c73c62694d9588f890
sylvain <sylvainkrieg@gmail.com>
2022-10-02 17:28:17 +0200

    add ch commands

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <future>
 #include <thread>
 #include <chrono>
+#include <unistd.h>
 
 using namespace std;
 
@@ -27,13 +28,28 @@ int main(int argc, char* argv[])
    // cmd->request_cmd();
 
 
-    auto enable_validation_mode_cmd = new Command_val_w(tr);
-    enable_validation_mode_cmd->request_cmd();
-    delete enable_validation_mode_cmd;
+//    auto enable_validation_mode_cmd = new Command_val_w(tr);
+//    enable_validation_mode_cmd->request_cmd();
+//    delete enable_validation_mode_cmd;
 
-    auto start_auto_tx = new Command_homologation_auto_tx(tr);
-    start_auto_tx->request_cmd();
-    delete start_auto_tx;
+
+//    auto start_auto_tx = new Command_homologation_auto_tx(tr);
+//    start_auto_tx->request_cmd();
+//    delete start_auto_tx;
+
+//    auto start_duty_cycle_mode = new Command_homologation_start_duty_cycle_mode(tr);
+//    start_duty_cycle_mode->request_cmd();
+//    delete start_duty_cycle_mode;
+
+//    auto continuous_cmd = new Command_homologation_start_continuous_mode(tr);
+//    continuous_cmd->request_cmd();
+//    delete continuous_cmd;
+//
+//    usleep(5*1000*1000);
+//
+    auto stop_homologation = new Command_homologation_stop_auto_tx(tr);
+    stop_homologation->request_cmd();
+    delete stop_homologation;
 
     return 0;
 }
